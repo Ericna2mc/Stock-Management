3a2ba009d69b74a4afbf741f62a1a9482c9efe4b
Ericna2mc <ericna.2mc@gmail.com>
2017-06-16 21:47:45 +0800

    Added Stock Management Project Source Code

---
+++ Stock Management/Stock Management/Login.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Stock_Management
+{
+    public partial class Login : Form
+    {
+       
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox1.Focus();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection("Data Source=eric-pc;Initial Catalog=Stock;User ID=sa;Password= NewCreation2017");
+            SqlDataAdapter sda = new SqlDataAdapter(@"SELECT *
+                  FROM [Stock].[dbo].[Login] where UserName = '"+textBox1.Text+"' and Password = '"+textBox2.Text+"'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                this.Hide();
+                StockMain1 main = new StockMain1();
+                main.Show();
+                
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username and Password..!", "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button1_Click(sender, e);
+            }
+           
+        }
+    }
+}
---
+++ Stock Management/Stock Management/StockMain1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Stock_Management
+{
+    public partial class StockMain1 : Form
+    {
+        public StockMain1()
+        {
+            InitializeComponent();
+        }
+
+        private void productsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Products pro = new Products();
+            pro.MdiParent = this;
+            pro.Show();
+        }
+
+        private void StockMain1_Load(object sender, EventArgs e)
+        {
+            //StockMain1 sm = new StockMain1();
+            //sm.Close();
+            //this.Close();
+
+           
+            
+            
+        }
+
+        private void menuStrip_Leave(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+    }
+}
